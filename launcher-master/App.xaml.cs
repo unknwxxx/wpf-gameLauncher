1c42a3e54f673410dd2066c6f2604d22f5120763
alexanderkhasaev <alexanderkhasaev@gmail.com>
2022-11-24 22:55:13 +0300

    loadingPage - 1

--- launcher-master/App.xaml.cs
+++ launcher-master/App.xaml.cs
@@ -12,10 +12,10 @@ namespace launcher_master
     {
         protected void ApplicationStart(object sender, StartupEventArgs e)
         {
-            //MainView mainWindow = new MainView();
-            //mainWindow.Show();
-            var LoginView = new LoginView();
-            LoginView.Show();
+            MainView mainWindow = new MainView();
+            mainWindow.Show();
+            //var LoginView = new LoginView();
+            //LoginView.Show();
             //LoginView.IsVisibleChanged += (s, ev) =>
             //{
             //    if (LoginView.IsVisible == false && LoginView.IsLoaded)
---
+++ launcher-master/ResourcesDictionary/Window.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace launcher_master.ResourcesDictionary
+{
+    partial class Window
+    {
+        public Window()
+        {
+            InitializeComponent();
+        }
+
+        private void MinimizeButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var window = (System.Windows.Window)((FrameworkElement)sender).TemplatedParent;
+            window.WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeRestoreButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var window = (System.Windows.Window)((FrameworkElement)sender).TemplatedParent;
+            window.WindowState = window.WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+        }
+
+        private void CloseButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var window = (System.Windows.Window)((FrameworkElement)sender).TemplatedParent;
+            window.Close();
+        }
+
+
+    }
+}
